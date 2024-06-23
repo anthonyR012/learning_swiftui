f98ce60c583ffb47bcd2387b59d8a4f50bbbff66
Anthony RUbio <rubionn27@gmail.com>
2024-06-23 16:06:59 -0500

    Initial Commit

---
+++ learning_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learning_swiftui
+//
+//  Created by Anthony Rubio on 23/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learning_swiftui/learning_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_swiftuiApp.swift
+//  learning_swiftui
+//
+//  Created by Anthony Rubio on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct learning_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
